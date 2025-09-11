f6cc566954e0f795285bf79975d87776e79bdcf6
Kaio <kaiocesa2009@gmail.com>
2025-09-10 23:54:31 -0300

    first commit

---
+++ TIPOS_DADOS_E_OPERADORES/script.js
@@ -0,0 +1,43 @@
+//1- Number
+console.log(typeof 2);
+console.log(typeof 25.14);
+console.log(typeof -125);
+console.log(5 + 9);
+console.log(5 - 9);
+console.log(5 * 9);
+console.log(5 / 9);
+console.log(5 + (45 * 2));
+
+//2- String Podemos usar aspas duplas ou simples e crase, mas cada um com suas pecualiaridades
+console.log(typeof "hello");
+console.log(typeof "hello world");
+console.log(typeof "123");
+
+
+// Simbolos especiais JS
+console.log("Olá kaio\ntudo bem?");
+console.log("Salve meu\t amigo");
+
+//Concatenação
+console.log("Olá " + "kaio");
+
+
+//3- Boolean
+console.log(typeof true);
+console.log(typeof false);
+
+//4- Null
+console.log(typeof null);
+
+//5- Undefined
+console.log(typeof undefined);
+
+//6- Symbol
+console.log(typeof Symbol());
+
+/* Special Numbers são dados considerados como numeros, mas não funcionam com eles: infinity, -infinity , NaN(Not a Number) */
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
+console.log(220 * "asd");
