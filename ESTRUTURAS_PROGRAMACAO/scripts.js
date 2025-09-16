28f505d9704728865d04f3cb992436c53f630b46
Kaio <kaiocesa2009@gmail.com>
2025-09-16 07:06:54 -0300

    Third commit

---
+++ ESTRUTURAS_PROGRAMACAO/scripts.js
@@ -0,0 +1,21 @@
+/* Variáveis */ /* Tipos de variáveis as varáveis let e const */
+let Username = "Kaio";
+let age = 24;
+
+Username = "Kaio França"; /* Case Sensitive */
+
+console.log(Username);
+console.log(age);
+
+const pi = 3.14;
+console.log(pi); /* Não podemos alterar o valor de uma constante */
+// pi = 3.1415; /* Isso gera um erro */
+
+/* Funções JS */
+let nome = prompt("Digite seu nome: ");
+
+/* Math */ /* Max e min e o floor(arredondamento) */
+console.log(Math.max(10, 15, 20, 8, 30));
+console.log(Math.min(10, 15, 18, 8, 72));
+console.log(Math.floor(4.9));
+console.log(Math.floor(4.1));
